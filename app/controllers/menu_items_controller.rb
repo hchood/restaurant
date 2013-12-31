a3406ec90bd778ce369a423968c90845c6e6c296
Helen Hood <helen.c.hood@gmail.com>
2013-12-31 11:28:40 -0500

    Feature specs for user_adds_menu_item are passing

--- app/controllers/menu_items_controller.rb
+++ app/controllers/menu_items_controller.rb
@@ -1,5 +1,9 @@
 class MenuItemsController < ApplicationController
 
+  def index
+
+  end
+
   def show
     @menu_item = MenuItem.find(params[:id])
   end
@@ -12,8 +16,11 @@ class MenuItemsController < ApplicationController
     # params[:menu_item][:price_in_cents] = params[:menu_item][:price_in_cents].to_i
 
     @menu_item = MenuItem.new(menu_item_params)
-    @menu_item.save
-    redirect_to menu_item_path(@menu_item)
+    if @menu_item.save
+      redirect_to menu_item_path(@menu_item)
+    else
+      render 'new'
+    end
   end
 
   private
