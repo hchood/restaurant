21522c0b8deb4d77411e597172d55798cb62d123
Helen Hood <helen.c.hood@gmail.com>
2013-12-31 15:07:27 -0500

    Now passing all tests

--- app/controllers/menu_items_controller.rb
+++ app/controllers/menu_items_controller.rb
@@ -30,8 +30,9 @@ class MenuItemsController < ApplicationController
   def update
     @menu_item = MenuItem.find(params[:id])
     if @menu_item.update(menu_item_params)
-      redirect_to menu_item_path(@menu_item)
+      redirect_to menu_item_path(@menu_item), notice: 'Menu item was successfully updated!! Hooray'
     else
+      render :edit
     end
 
   end
